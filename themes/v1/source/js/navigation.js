e27e0483f803b3d70285c0f1a7dc5c498c56663f
Eric Range <mail@eric-range.de>
2016-06-21 13:03:21 +0200

    Update

--- themes/v1/source/js/navigation.js
+++ themes/v1/source/js/navigation.js
@@ -1,5 +1,8 @@
-var mobileNav = document.getElementById('mobile-nav');
-mobileNav.addEventListener('click', function() {
-  var navItems = document.getElementsByClassName('nav-items')[0];
-  navItems.classList.toggle('mobile-nav');
-}, false);
+(function() {
+  "use strict";
+  var mobileNav = document.getElementById('mobile-nav');
+  mobileNav.addEventListener('click', function() {
+    var navItems = document.getElementsByClassName('nav-items')[0];
+    navItems.classList.toggle('mobile-nav');
+  }, false);
+})();
--- themes/v1/source/js/sign-up.js
+++ themes/v1/source/js/sign-up.js
@@ -1,26 +1,35 @@
-var body = document.getElementsByTagName('body')[0];
-var signUp = document.getElementById('sign-up-modal');
+(function() {
+  "use strict";
+  var body = document.getElementsByTagName('body')[0];
+  var signUp = document.getElementById('sign-up-modal');
 
-var signUpButtons = document.getElementsByClassName('sign-up');
-for (var i = 0; i < signUpButtons.length; i++) {
-  signUpButtons[i].addEventListener('click', showSignUp, false);
-}
-
-function showSignUp(event) {
-  if (event && event.preventDefault) {
-    event.preventDefault();
-  } else if (window.event && window.event.returnValue) {
-    window.event.returnValue = false;
+  var signUpButtons = document.getElementsByClassName('sign-up');
+  for (var i = 0; i < signUpButtons.length; i++) {
+    signUpButtons[i].addEventListener('click', showSignUp, false);
   }
 
-  event.stopPropagation();
+  function showSignUp(event) {
+    if (typeof event !== 'undefined' && event && event.preventDefault) {
+      event.preventDefault();
+      event.stopPropagation();
 
-  body.classList.add('sign-up');
-  signUp.classList.add('active');
-  signUp.querySelector('.outer').addEventListener('click', hideSignUp, false);
-}
+    } else if (typeof window.event !== 'undefined' && window.event && window.event.returnValue) {
+      window.event.returnValue = false;
 
-function hideSignUp() {
-  body.classList.remove('sign-up');
-  signUp.classList.remove('active');
-}
+    }
+
+    body.classList.add('sign-up');
+    signUp.classList.add('active');
+    signUp.querySelector('.outer').addEventListener('click', hideSignUp, false);
+  }
+
+  function hideSignUp() {
+    body.classList.remove('sign-up');
+    signUp.classList.remove('active');
+  }
+
+  /* Detect if showSignUp() should be triggered. */
+  if (location.hash.indexOf('#!/sign-up') > -1) {
+    showSignUp();
+  }
+})();
